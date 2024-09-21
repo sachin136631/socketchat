adc0afe484802f8001097cc899a35a5698c919e3
sachin <sachinmanojkr@gmail.com>
2024-09-21 10:34:25 +0530

    second commit

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.get("/",(req,res)=>{
 });
 
 io.on("connection",(socket)=>{
-    console.log("a user connected");
+    socket.on("chat message",(msg)=>{
+        io.emit("chat message",msg);
+    });
 });
 
 socket.on("disconnect",()=>{
