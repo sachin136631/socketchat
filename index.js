6a1e5d6566f2cccc920bb884f017177c59f77b6f
sachin <sachinmanojkr@gmail.com>
2024-09-22 19:10:23 +0530

    added state recovery functionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {createServer} from "node:http";
-import {server} from "socket.io";
+import {Server} from "socket.io";
 const app=express();
 const server=createServer(app);
-const io=new Server(server);
+const io=new Server(server,{
+    connectionStateRecovery:{}
+});
 
 app.use(express.static("public"));
 
@@ -16,11 +18,11 @@ io.on("connection",(socket)=>{
     socket.on("chat message",(msg)=>{
         io.emit("chat message",msg);
     });
+    socket.on("disconnect",()=>{
+        console.log("user disconnected");
+    });
 });
 
-socket.on("disconnect",()=>{
-    console.log("user disconnected");
-});
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
